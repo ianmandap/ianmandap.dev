a8bf67449c0e601cd4f15bb0b99848287f440492
theIanMilan <mandap.fmg@gmail.com>
2025-04-08 19:15:22 +0800

    Add favicon and career shift blog

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,6 +67,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       className={`${space_grotesk.variable} scroll-smooth`}
       suppressHydrationWarning
     >
+      <link rel="icon" type="image/svg+xml" href={`${basePath}/static/favicons/favicon.svg`} />
       <link
         rel="apple-touch-icon"
         sizes="76x76"
@@ -75,14 +76,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <link
         rel="icon"
         type="image/png"
-        sizes="32x32"
-        href={`${basePath}/static/favicons/favicon-32x32.png`}
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href={`${basePath}/static/favicons/favicon-16x16.png`}
+        sizes="any"
+        href={`${basePath}/static/favicons/favicon.ico`}
       />
       <link rel="manifest" href={`${basePath}/static/favicons/site.webmanifest`} />
       <link
