2e539a691b57679dc1a85cd9dee7e468fad5d612
theIanMilan <mandap.fmg@gmail.com>
2025-04-09 04:58:15 +0800

    Minor edit to career-shift blog and make absolute URLs work for Images

--- components/Image.tsx
+++ components/Image.tsx
@@ -2,8 +2,10 @@ import NextImage, { ImageProps } from 'next/image'
 
 const basePath = process.env.BASE_PATH
 
-const Image = ({ src, ...rest }: ImageProps) => (
-  <NextImage src={`${basePath || ''}${src}`} {...rest} />
-)
+const Image = ({ src, ...rest }: ImageProps) => {
+  const imageSource = src.toString().startsWith('/') ? `${basePath || ''}${src}` : src
+
+  return <NextImage src={imageSource} {...rest} />
+}
 
 export default Image
